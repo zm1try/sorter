56ff47af735f6d5eae1babbe50ff46d1c853ae41
zm1try <zmitryarosh@gmail.com>
2018-02-23 01:28:17 +0300

    19/25

--- src/index.js
+++ src/index.js
@@ -1,30 +1,54 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.data = [];
+    this.comparator = (left, right) => left - right;
   }
 
   add(element) {
-    // your implementation
+    this.data.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.data[index];
   }
 
   get length() {
-    // your implementation
+    return this.data.length;
   }
 
   toArray() {
-    // your implementation
+    return this.data;
   }
 
   sort(indices) {
-    // your implementation
+    this.indices = indices;
+    let temp;
+    for(let i = 0; i<this.indices.length;i++) {
+      for(let j = i; j<this.indices.length-i;j++) {
+        if (this.indices[j]>this.indices[j+1]) {
+          temp = this.indices[j+1];
+          this.indices[j+1] = this.indices[i];
+          this.indices[j] = temp;
+        }
+      }
+    }
+    
+    for(let i = 0; i<this.indices.length;i++) {
+      for(let j = 0; j<this.indices.length;j++) {
+        if (this.comparator(this.data[this.indices[j]],this.data[this.indices[j+1]])>0) {
+          console.log(this.comparator(this.data[this.indices[j]],this.data[this.indices[j+1]]));
+          temp = this.data[this.indices[j]+1];
+          this.data[this.indices[j+1]] = this.data[this.indices[j]];
+          this.data[this.indices[j]] = temp;
+        }
+      }
+    }
+    
+
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comparator = compareFunction;
   }
 }
 
